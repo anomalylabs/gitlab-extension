ff980537fb205b9189151bff324c154b075537fd
Ryan Thompson <service@aiwebsystems.com>
2019-01-08 15:36:20 -0600

    Link

--- resources/lang/en/section.php
+++ resources/lang/en/section.php
@@ -3,6 +3,6 @@
 return [
     'personal_access_token' => [
         'title'       => 'Personal Access Token',
-        'description' => 'You can generate a new token at <a href="https://gitlab.com/settings/tokens" target="_blank">GitLab</a>.',
+        'description' => 'You can generate a new token at <a href="https://gitlab.com/profile/personal_access_tokens" target="_blank">GitLab</a>.',
     ],
 ];
